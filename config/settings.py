95d64bbb0e8846113e933a1221e508ba333f1d02
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.comclear>
2025-06-27 21:51:20 +0200

    Add BookingForm and update settings.py to register templates and app

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+if os.path.exists("env.py"):
+import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n1aroke!^7a44_r)#w@dgc%%o+1iz_f#5ka!^-)p*bzgk29k#('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cuisine',
+    'bookings',
+    
 ]
 
 MIDDLEWARE = [
